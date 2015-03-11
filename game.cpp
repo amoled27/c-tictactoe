aec5af55387e06fa9bc719f7b7f5fbcda5225a98
amoled27 <amodshinde1995@gmail.com>
2015-03-11 22:23:34 +0530

    updated logic, bugs left

--- game.cpp
+++ game.cpp
@@ -1,15 +1,17 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 class display{
 
-
-int a=1,b=2,c=3,d=4,e=5,f=6,g=7,h=8,i=9;
+int j;
+char a=49,b=50,c=51,d=52,e=53,f=54,g=55,h=56,i=57;
 char lcorner_top = 201,h_line= 205, mid_t=203,rcorner_top=187; //lcorner is left corner, rcorner is right
 char v_line=186,lcorner_b =200 , rcorner_b =188, pluss=206,mid_l=204,mid_r=185,mid_b= 202;  // lcorner_b is left corner bottom
 
 public:
     void outline(){
+
         // topmost line of box
         cout<<lcorner_top<<h_line<<h_line<<h_line<<h_line<<mid_t<<h_line<<h_line<<h_line<<h_line
             <<mid_t<<h_line<<h_line<<h_line<<h_line<<rcorner_top<<endl;
@@ -35,14 +37,82 @@ public:
         cout<<lcorner_b<<h_line<<h_line<<h_line<<h_line<<mid_b<<h_line<<h_line<<h_line<<h_line
             <<mid_b<<h_line<<h_line<<h_line<<h_line<<rcorner_b;
 }
+// fill data
+int filldata(int j,int k){
+int temp=0;
+switch(j){
+case 1:
+    a=k;
+    break;
+case 2:
+    b=k;
+    break;
+case 3:
+    c=k;
+    break;
+case 4:
+    d=k;
+    break;
+case 5:
+    e=k;
+    break;
+case 6:
+    f=k;
+    break;
+case 7:
+    g=k;
+    break;
+case 8:
+    h=k;
+    break;
+case 9:
+    i=k;
+    break;
+/*case: default
+    cout<<"\nEnter proper position";*/
+}
+//checking win condition
+temp=win_check(a,b,c,d,e,f,g,h,i);
+return(temp);
+}
 
+void pos(){
+    int j,flag=1,k=120,temp;
+for(int i=0;i<9;i++){
+        if(i%2==0){
+           flag=1;
+           k=120;}
+        else {
+            flag=2;
+            k=111;}
+ cout<<"\nplayer"<<flag<<" enter the position\n"<<endl;
+cin>>j;
+    temp=filldata(j,k);
+    outline();
+    if(temp==1)
+        break;
+}
+}
 
-
+int win_check(int a,int b,int c,int d,int e,int f,int g,int h,int i){
+    int flag=0;
+if((a&&b&&c)==120|| (a&&d&&g)==120||(a&&e&&i)==120||(d&&e&&f)==120||(g&&h&&i)==120||(c&&e&&g)==120||(b&&e&&h)==120||(c&&f&&i)==120){
+    cout<<"player 1 wins"<<endl;
+    flag=1;
+}
+    else if((a&&b&&c)==111|| (a&&d&&g)==111||(a&&e&&i)==111||(d&&e&&f)==111||(g&&h&&i)==111||(c&&e&&g)==111||(b&&e&&h)==111||(c&&f&&i)==111){
+    cout<<"player 2 wins"<<endl;
+    flag=1;
+    }
+    return(flag);
+}
 
 };
 
 int main(){
 display d;
 d.outline();
+d.pos();
+//clrscr();
 return 0;
 }
